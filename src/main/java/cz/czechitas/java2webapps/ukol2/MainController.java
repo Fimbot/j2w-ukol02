eee6498109a993a21236ba72daad8eea9078b222
Filip Jirsák <filip@jirsak.org>
2021-04-12 17:57:52 +0200

    Řešení úkolu.

--- src/main/java/cz/czechitas/java2webapps/ukol2/MainController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol2/MainController.java
@@ -1,15 +1,20 @@
 package cz.czechitas.java2webapps.ukol2;
 
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
@@ -24,28 +29,30 @@ public class MainController {
   private final List<String> messages;
   private final List<String> images;
 
-  public MainController() throws URISyntaxException, IOException {
-    messages = readAllLines("citaty.txt");
+  public MainController(ResourceLoader resourceLoader) throws URISyntaxException, IOException {
+    Resource citatyResource = resourceLoader.getResource("classpath:/citaty.txt");
+    Resource obrazkyResource = resourceLoader.getResource("classpath:/obrazky.txt");
 
-    images = Arrays.asList("95YRwf6CNw8", "m_HRfLhgABo", "xrVDYZRGdw4", "UWRqlJcDCXA", "CDbB4EJ4i_A", "uEcSKKDB1pg", "1td5Iq5IvNc", "70Rir5vB96U", "XJXWbfSo2f0",
-            "fPkvU7RDmCo", "L2cxSuKWbpo", "Skf7HxARcoc", "f7xiGC81KTE");
+    messages = readAllLines(citatyResource);
+    images = readAllLines(obrazkyResource);
   }
 
   @GetMapping("/")
   public ModelAndView home() {
     String imageURL = String.format("https://source.unsplash.com/%s/1600x900", images.get(random.nextInt(images.size())));
-    String bodyStyle = String.format("background-image: url(%s);", imageURL);
 
     ModelAndView result = new ModelAndView("index");
     result.addObject("message", messages.get(random.nextInt(messages.size())));
-    result.addObject("bodyStyle", bodyStyle);
+    result.addObject("image", imageURL);
     return result;
   }
 
-  private static List<String> readAllLines(String resource) throws IOException {
-    ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-    try (InputStream inputStream = classLoader.getResourceAsStream(resource);
-         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+  private static List<String> readAllLines(Resource resource) throws IOException {
+    try (
+            InputStream inputStream = resource.getInputStream();
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+            BufferedReader reader = new BufferedReader(inputStreamReader)
+    ) {
       return reader
               .lines()
               .collect(Collectors.toList());
